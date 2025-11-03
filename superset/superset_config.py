90901a6d21a7aa2f97f8061c9bbb478f02185a57
Durvesh Narwelkar <durveshnarwelkar@gmail.com>
2025-11-03 12:27:25 +0530

    Update superset_config.py

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -1,7 +1,7 @@
 import os
 
 # A strong random secret key (you can generate with: openssl rand -base64 42)
-SECRET_KEY = ''
+SECRET_KEY = os.environ.get("SUPERSET_SECRET_KEY", "replace-me-with-secure-random")
 
 # SQLAlchemy connection string for the metadata database (using SQLite for now)
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:<password>@localhost/storage_metadata'
